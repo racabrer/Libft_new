dada884767d9268cb1760729a47ee5e3947d73cd
Raquel Cabrero <cabrero.raquel@gmail.com>
2026-03-09 21:11:07 +0100

    memmove

--- memmove.c
+++ memmove.c
@@ -1,20 +1,38 @@
+/*
+Copia un bloque de memoria de una ubicación a otra,
+incluso si los bloques de memoria se superponen.
+
+Hay solapamiento cuando el buffer de origen es menor que el buffer
+de destino, esto es cuando se encuentra en una posición de memoria
+anterior. 
+Para evitar una pérdida de información, una solución es copiar comenzando
+por el final en lugar de por el principio.
+*/
 #include <stddef.h>
 
-void *ft_memset(void *s, int c, size_t n)
+void *ft_memmove(void *dest, void *src, size_t n)
 {
-    unsigned char *p;
-
-    p = (unsigned char *)s;
-    while(n > 0)
+    size_t i;
+    
+    if (!dest && !src) //Comprobación de punteros nulos
+        return (0);
+    i = 0;
+    if ((size_t)dest - (size_t)src < n) //decidir el sentido de la copia
     {
-        *p = (unsigned char *)c;
-        p++;
-        n--;
+        i = n - 1;
+        while(i < n)
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i--;
+        }
     }
-    return (s); 
+    else
+    {
+        while(i < n)
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+    }
+    return (dest);
 }
-
-/*
-Devuelve s porque es void * que es un puntero a algo 
-que no tiene tipo concreto. Devuelve el puntero genérico del principio
-*/
